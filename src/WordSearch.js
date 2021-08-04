e174c37b5378f1509f2ed2dde5ad708b1a9a4398
dimbee40 <78416865+dimbee40@users.noreply.github.com>
2021-08-04 14:22:26 +1000

    dummy dictionary fields setStates

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -1,41 +1,25 @@
 import React, { useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 
 export default function WordSearch() {
   const [word, setWord] = useState("");
-  const [temperature, setTemperature] = useState("");
-  const [wind, setWind] = useState("");
-  const [icon, setIcon] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [description, setDescription] = useState("");
+  const [pronounciation, setPronounciation] = useState("");
+  const [definition, setDefinition] = useState("");
 
   function handleResponse(response) {
     console.log(response);
-    setTemperature(
+    setWord(<li>{word}</li>);
+    setPronounciation(<li>pronunciation: {pronounciation}</li>);
+    setDefinition(
       <li>
-        {word} is {Math.round(response.data.main.temp)} °C
-      </li>
-    );
-    setDescription(
-      <li>Description: {response.data.weather[0].description}</li>
-    );
-    setWind(<li>Windspeed: {response.data.wind.speed} km/h</li>);
-    setHumidity(<li>Humidity:{Math.round(response.data.main.humidity)}%</li>);
-    let iconURL = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-
-    setIcon(
-      <li>
-        {" "}
-        <img src={iconURL} alt={description} />{" "}
+        {word} means {definition}{" "}
       </li>
     );
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    let api = "3ec119a7b4622feedeeba843b106eb0a";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${word}&appid=${api}&units=metric`;
-    axios.get(url).then(handleResponse);
+    handleResponse();
   }
 
   function changeWord(event) {
@@ -49,11 +33,10 @@ export default function WordSearch() {
         <input type="search" placeholder="enter a word" onChange={changeWord} />
         <input type="submit" value="Search for word" />
       </form>
-      <p>{temperature}</p>
-      <p>{wind}</p>
-      <p>{humidity}</p>
-      <p>{description}</p>
-      <p>{icon}</p>
+      <p>{word}</p>
+      <p>{pronounciation}</p>
+      <p>{definition}</p>
+
       <div className="definition">
         <h2> meaning </h2>
         <p>A definition is the meaning of a word </p>
