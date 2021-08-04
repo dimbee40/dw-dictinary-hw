4d30627dd891ddc86a8dc264ef75d703b196ca1f
dimbee40 <78416865+dimbee40@users.noreply.github.com>
2021-08-04 10:50:09 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary Search</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
